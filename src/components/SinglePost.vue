d428be6574f6ece063fd61379a25ee05ccd74462
rjmcbain <rmcbain01@gmail.com>
2021-03-02 13:01:44 -0800

    great example of passing props

---
+++ src/components/SinglePost.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="post">
+    <h3>{{ post.title }}</h3>
+    <p>{{ post.body }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["post"],
+};
+</script>
+
+<style></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="home">
+    <h1>Home</h1>
+    <PostList :posts="posts" />
+  </div>
+</template>
+
+<script>
+import PostList from "../components/PostList";
+import { ref } from "@vue/reactivity";
+export default {
+  name: "Home",
+  components: { PostList },
+  setup() {
+    const posts = ref([
+      {
+        title: "Welcome to the blog",
+        body:
+          "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur quod repellat recusandae reiciendis, laboriosam labore sed aliquid quisquam esse quos.",
+        id: 1,
+      },
+      { title: "Top 5 CSS tips", body: "Lorem ipsum", id: 2 },
+    ]);
+    return { posts };
+  },
+};
+</script>
+
+<style></style>
